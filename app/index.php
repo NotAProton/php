11c9dbbaca535ed8c6d0a0730f188940e9067806
Not A Proton <notaproton8@gmail.com>
2024-03-21 14:57:27 +0530

    Create first webage

---
+++ app/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello!</title>
+</head>
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        text-align: center;
+        background-color: #171717;
+        color: #FDFDFD;
+        margin-top: 20%;
+        font-size: 20px;
+    } 
+</style>
+<body>
+    <h1>Hiii</h1>
+    <p>Welcome to my webpage :D</p>
+    <p>Your IP address is: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+    <p>:3</p>
+    </body>
+</html>
